87657e75d77dfe7ee16f707ca8a651af00c11414
Julian.zhang <flesharcher@hotmail.com>
2013-07-04 16:31:39 +0800

    combine web and log parse as demo

--- web/main.clj
+++ web/main.clj
@@ -1,13 +1,36 @@
 (ns web.main
     (:use ring.adapter.jetty)
+    (:use clojure.java.io)
+    (:use log-consumer.logparser)
     (:gen-class)
+)
+
+(def  logcahe (atom ["12","23"])
 )
 
 (defn app
     [{:keys [uri]}]
-    {:body (format "You requested %s" uri)}
+    {:body 
+        (format "You requested %s" 
+            (count
+                (parse-log parse-rules @logcahe)
+            )
+        )
+
+    }
 )
 
 (defn -main []
+    (println logcahe)
     (run-jetty #'app {:port 8085 :join? false})
+    (Thread/sleep 20000)
+    (println "sleep over")
+    (reset! logcahe 
+        (.split
+            (slurp "/Users/zhangjun/Desktop/code/fs/log_analyzer/log_consumer/test.log") 
+         "\n"
+        )
+    )
+    (println (count @logcahe))
 )
+
