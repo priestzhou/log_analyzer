ebda76bd83fc0b8030b81255a754dfc57958c2f3
Julian.zhang <flesharcher@hotmail.com>
2013-07-05 15:38:55 +0800

    add reload framework and reload from file

---
+++ log_consumer/logconsumer.clj
@@ -0,0 +1,25 @@
+(ns log-consumer.logconsumer
+    (:use log-consumer.logparser)
+)
+
+(defn wait-and-run [log-atom log-reload]
+    (Thread/sleep 5000)
+    (log-reload log-atom)
+    (recur log-atom log-reload)
+)
+
+(defn reload-from-file [log-atom]
+    (let [fpath "/Users/zhangjun/Desktop/test.log"]
+        (->>
+            (slurp fpath)
+            (#(.split % "\n"))
+            (parse-log parse-rules)
+                        (filter 
+                #(<  1 (count %))
+            )
+            concat
+            first
+            (reset! log-atom)
+        )
+    )
+)
