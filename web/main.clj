70c05e410c2fb4abc1e8e67841045059233a5c13
Julian.zhang <flesharcher@hotmail.com>
2013-07-17 12:27:53 +0800

    add ajax demo

--- web/main.clj
+++ web/main.clj
@@ -12,19 +12,38 @@
     (atom [])
 )
 
+
+(comment def html (slurp "d3_experiments1.html"))
+
+
+(defn- get-jar []
+    (->>
+        (new web.main)
+        (#(. % getClass))
+        (#(. % getProtectionDomain))
+        (#(. % getCodeSource))
+        (#(. % getLocation))
+        (#(. % toString))
+    )
+)
+
 (defn app
     [{:keys [uri]}]
     {:body
-        (if (= uri "dataflow")
-            (gen-json @logdata)
-            ()
-        ) 
+        (if (= uri "/d3")
+            (slurp 
+                (.getResourceAsStream (.getClass System) "/web/d3_experiments1.html")
+            )
+            (gen-json @logdata) 
+        )
         
     }
 )
 
+
 (defn -main []
     (run-jetty #'app {:port 8085 :join? false})
+
     (wait-and-run logdata reload-from-file)
 )
 
