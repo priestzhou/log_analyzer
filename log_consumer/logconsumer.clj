015a8a7f09bcab6de0d955d26539c4fbdd23da6c
zhangjun <zhangjun.julian@gmail.com>
2013-07-24 17:21:02 +0800

    delete useless function

--- log_consumer/logconsumer.clj
+++ log_consumer/logconsumer.clj
@@ -13,23 +13,7 @@
     )
 )
 
-(defn wait-and-run [log-atom log-reload]
-    (Thread/sleep 5000)
-    (log-reload log-atom)
-    (recur log-atom log-reload)
-)
 
-(defn reload-from-file [log-atom]
-    (let [fpath "/Users/zhangjun/Desktop/test.log"]
-        (->>
-            (slurp fpath)
-            (#(.split % "\n"))
-            (parse-log parse-rules)
-            after-parse
-            (reset! log-atom)
-        )
-    )
-)
 (defn- parse-step-kfk [logstep]
     (->>
         logstep
