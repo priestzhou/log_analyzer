fd337e7432461f30edbdba35fb5842c1159be571
zhangjun <zhangjun.julian@gmail.com>
2013-07-24 14:53:39 +0800

    add the argparser

--- web/main.clj
+++ web/main.clj
@@ -2,7 +2,10 @@
     (:use ring.adapter.jetty)
     (:use clojure.java.io)
     (:use log-consumer.logparser)
-    (:use log-consumer.logconsumer)  
+    (:use log-consumer.logconsumer)
+    (:require
+        [argparser.core :as arg]
+    )  
     (:gen-class)
 )
   
@@ -33,13 +36,45 @@
     }
 )
 
-(defn -main []
-    (run-jetty #'app {:port 8085 :join? false})
-    (consumer-from-kfk 
-        "115.28.40.198:2181" 
-        "hdfs.data-node" 
-        "data-consumer24"
-        logdata
+(defn -main [& args]
+    (let [arg-spec 
+            {
+                :usage "Usage: zookeeper [topic] [group] exejar"
+                :args [
+                    (arg/opt :help
+                        "-h|--help" "show this help")
+                    (arg/opt :zkp 
+                        "-zkp <ip:port>" "the zookeeper ip & port")
+                    (arg/opt :topic
+                        "-topic <topic>" "the kafka topic")
+                    (arg/opt :group
+                        "-group <group>" "the consumer group")
+                    ]
+            }
+            opts (arg/transform->map (arg/parse arg-spec args))
+            default-args 
+                {
+                    :topic ["hdfs.data-node"]
+                    :group ["data-node-consumer"]
+                }
+            opts-with-default (merge default-args opts)
+        ]
+        (when (:help opts)
+            (println (arg/default-doc arg-spec))
+            (System/exit 0)            
+        )
+        (when (not (:zkp opts))
+            (println "please input the zookeeper info")
+            (println (arg/default-doc arg-spec))
+            (System/exit 0)            
+        )     
+        (run-jetty #'app {:port 8085 :join? false})
+        (consumer-from-kfk 
+            (first (:zkp opts-with-default))
+            (first (:topic opts-with-default))
+            (first (:group opts-with-default))
+            logdata
+    )
     )
 )
 
