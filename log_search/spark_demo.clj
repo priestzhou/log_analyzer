c70d9b57263209daa7f815293edce5d05e509003
zhangjun <zhangjun.julian@gmail.com>
2013-08-09 09:45:56 +0800

    lib adding

--- log_search/spark_demo.clj
+++ log_search/spark_demo.clj
@@ -1,8 +1,23 @@
-(ns log-search.spark_demo
+(ns log-search.spark-demo
     (:require 
         [serializable.fn :as sfn]
         [clj-spark.api :as k]
     )
+    (:gen-class)
 )
 
-()
+(defn -main []
+    (let [sc (k/spark-context 
+                :master "192.168.1.100:7077" :job-name "Simple Job" 
+            )
+            input-rdd (.textFile sc "/Users/zhangjun/Desktop/code/fs/search/
+                log-collector.log.2013-08-06"
+                )
+        ]
+        (-> input-rdd
+            (k/count)
+            println
+        )
+    )
+    
+)
