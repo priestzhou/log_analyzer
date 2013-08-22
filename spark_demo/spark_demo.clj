7e79810079c7adff09ddb8bc4e3cfb35260c47d6
zhangjun <zhangjun.julian@gmail.com>
2013-08-22 11:41:43 +0800

    can't run

--- spark_demo/spark_demo.clj
+++ spark_demo/spark_demo.clj
@@ -12,57 +12,40 @@
     (:gen-class)
 )
 
-(comment defn -main []
-    (->>
-        (JavaSparkContext. 
-            "spark://192.168.1.100:7077" 
-            "test job" 
-            "/home/admin/spark-0.7.3"
-            "/Users/zhangjun/temp.hs"
-        )
-        (#(.textFile % "/etc/hosts"))
-        (#(.map % 
-            (sfn/fn [log]
-                ({:message log})
-            )
-        ))
-        (#(.collect %))
-        (into [] )
-        println
-        ;(#(map println %))
-        ;dorun
-    )
-)
-
-(defn -main []
+(defn get-test-rdd []
     (let [sc (k/spark-context 
                 :master "spark://192.168.1.100:7077" :job-name "Simple Job" 
                 :spark-home "/home/admin/spark-0.7.3" 
-                :jars ["./clj_spark_rebuild.jar" "./spark_demo.jar"
+                :jars ["./spark_demo.jar" "./clj_spark_rebuild.jar" 
                     "./log_search.jar"] ;
                 )
             input-rdd (.textFile sc "/logfile"
                 )
-            testp (lsp/parse-all "*hdfs*")
-        ]
-        (->
+            ]
+        (k/map 
             input-rdd
-            (k/map 
-                (sfn/fn f [log]
-                    (json/read-str log :key-fn keyword)
-                )
+            (sfn/fn f [log]
+                (json/read-str log :key-fn keyword)
             )
-            (#(spe/do-search testp %))
-            (#(k/first %))
-            println
         )
     )
 )
 
-            (comment k/map 
-                (sfn/fn f1 [log]
-                    [(get log "pcid") log]
-                )
-            k/group-by-key
-            (k/map second)
-            )
+(defn run-test [inStr]
+    (let [testrdd (get-test-rdd)
+            tp (lsp/parse-all inStr)
+        ]
+    (->>
+        testrdd
+        (spe/do-search tp)
+        println
+    )        
+    )
+)
+
+(defn -main []
+    (do
+        (run-test "*hdfs*")
+        ;(run-test "*hdfs_* | parse \"HDFS_*\" as type | count type by type ")
+    )
+)
