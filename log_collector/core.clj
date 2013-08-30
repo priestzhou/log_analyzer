561832df300d337d2a868069941809d9cf2e802d
taoda <taoyfeng@gmail.com>
2013-08-30 13:46:18 +0800

    refine

--- log_collector/core.clj
+++ log_collector/core.clj
@@ -44,49 +44,85 @@
     )
 )
 
-(defn- main-loop [producer opts]
-    (while true
+(defn- produce 
+    ([producer logs cnt]
+        (if (empty? logs)
+            cnt
+            (let [[bulk remains] (split-at 1000 logs)]
+                (kfk/produce producer bulk)
+                (if-let [{:keys [topic message]} (first bulk)]
+                    (info "Sent logs" 
+                        :count (count bulk)
+                        :first {
+                            :topic topic 
+                            :message (-> message
+                                (String. StandardCharsets/UTF_8)
+                                (json/read-str)
+                            )
+                        }
+                    )
+                )
+                (Thread/sleep 100)
+                (recur producer remains (+ cnt (count bulk)))
+            )
+        )
+    )
+
+    ([producer logs]
         (try
-            (let [logs (for [
-                    [k v] opts
-                    f (->> (scan v)
-                        (take 2)
-                        (reverse)
+            (if (empty? logs)
+                (info "Find no new logs")
+                (do
+                    (info "Find new logs")
+                    (let [total (produce producer logs 0)]
+                        (info "Sent all new logs. Wait for 5 secs." :total total)
                     )
-                    ln (read-logs v f)
-                    :let [not-cached-ln (llp/cache-log-line ln)]
-                    :when not-cached-ln
-                    :let [message (-> not-cached-ln
-                        (json/write-str)
-                        (.getBytes (StandardCharsets/UTF_8))
-                    )]
-                    ]
-                    {:topic (name k) :message message}
                 )
-                ]
-                (info "Find new logs")
-                (doseq [plogs (partition-all 1000 logs)]
-                    (kfk/produce producer plogs)
-                    (if-let [{:keys [topic message]} (first plogs)]
-                        (info "Sent logs" 
-                            :count (count plogs)
-                            :first {
-                                :topic topic 
-                                :message (-> message
-                                    (String. StandardCharsets/UTF_8)
-                                    (json/read-str)
-                                )
-                            }
-                        )
-                    )
-                    (Thread/sleep 100)
-                )
-                (info "Sent all new logs. Wait for 5 secs.")
             )
         (catch IOException ex
             (error "IO error. Wait for 5 secs." :exception ex)
         ))
-        (Thread/sleep 5000)
+    )
+)
+
+(defn- fetch-logs [opts file-info]
+    (try
+        (let [logs (for [
+                [k v] opts
+                f (->> (scan v)
+                    (take 2)
+                    (reverse)
+                )
+                ln (read-logs v f)
+                :let [not-cached-ln (llp/cache-log-line ln)]
+                :when not-cached-ln
+                :let [message (-> not-cached-ln
+                    (json/write-str)
+                    (.getBytes (StandardCharsets/UTF_8))
+                )]
+                ]
+                {:topic (name k) :message message}
+            )
+            ]
+            [logs {}]
+        )
+    (catch IOException ex
+        (error "IO error. Wait for 5 secs." :exception ex)
+        nil
+    ))
+)
+
+(defn- main-loop [producer opts file-info]
+    (if-let [[logs new-file-info] (fetch-logs opts file-info)]
+        (do
+            (produce producer logs)
+            (Thread/sleep 5000)
+            (recur producer opts new-file-info)
+        )
+        (do
+            (Thread/sleep 5000)
+            (recur producer opts file-info)
+        )
     )
 )
 
@@ -94,7 +130,7 @@
     (while true
         (try
             (with-open [producer (kfk/newProducer (:kafka opts))]
-                (main-loop producer (dissoc opts :kafka))
+                (main-loop producer (dissoc opts :kafka) {})
             )
         (catch KafkaException ex
             (error "Cannot create kafka producer. Wait for 15 secs." :exception ex)
